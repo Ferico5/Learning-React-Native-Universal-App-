b276087f3bba4d4c6bee74339de93d986a55e3a0
ferico <huangferico@gmail.com>
2025-11-10 21:07:27 +0700

    learning pick image

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,13 +4,14 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 type labelProps = {
   label: string;
   theme?: 'primary';
+  onPress?: () => void;
 };
 
-export default function Button({ label, theme }: labelProps) {
+export default function Button({ label, theme, onPress }: labelProps) {
   if (theme === 'primary') {
     return (
       <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 }]}>
-        <Pressable style={[styles.button, { backgroundColor: '#fff' }]} onPress={() => alert('You pressed a button')}>
+        <Pressable style={[styles.button, { backgroundColor: '#fff' }]} onPress={onPress}>
           <FontAwesome name="picture-o" style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
         </Pressable>
@@ -20,7 +21,7 @@ export default function Button({ label, theme }: labelProps) {
 
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+      <Pressable style={styles.button} onPress={onPress}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
